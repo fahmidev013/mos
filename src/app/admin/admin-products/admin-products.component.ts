3bfd1db03d020deeb74cb2dc544aa4dfe5c0ad40
fahmidev013 <flasherid@gmail.com>
2019-05-31 09:36:05 +0700

    add search filtur

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,15 +1,18 @@
-import { Observable } from 'rxjs';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 import { ProductService } from './../../services/product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent implements OnInit, OnDestroy {
 
-  products$: Observable<any>;
+  products: any[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
   constructor(
     private productSvc: ProductService
@@ -18,7 +21,20 @@ export class AdminProductsComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.products$ = this.productSvc.getAll();
+    this.subscription = this.productSvc.getAll()
+      .subscribe(products => this.filteredProducts = this.products = products);
   }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  filter(query: string) {
+    this.filteredProducts = (query) ?
+      this.products.filter(p => p['title'].toLowerCase().includes(query.toLowerCase())) :
+      this.products;
+  }
+
+
 
 }
